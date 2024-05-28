0ab70a2f23a92fbe1c71be9fa96cfa323d3c09f7
balu627 <baluvamsikrishna627@gmail.com>
2024-05-28 20:30:51 +0530

    insterted api data to sql

--- server.js
+++ server.js
@@ -1,14 +1,49 @@
 const url = require("url");
 const http = require("http");
+const mysql = require("mysql");
+
+const dbconfig = {
+  host: "localhost",
+  user: "root",
+  password: "123456789",
+  database: "project"
+};
 
 const host = "127.0.0.1";
 const port = 8000;
+
+const connection = mysql.createConnection(dbconfig);
+
+connection.connect(function (err){
+  if (err) {
+    console.error("Error connecting to MySQL:", err);
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
 
 async function apifetch() {
   const response = await fetch(
     "https://opentdb.com/api.php?amount=10&category=11&difficulty=easy&type=multiple"
   );
   const responsejs = await response.json();
+  const result = responsejs.results;
+  result.forEach(singleres =>{
+    let question  = singleres.question;
+    let options= singleres.incorrect_answers+",";
+    options+=singleres.correct_answer;
+    const sqlQuery = "INSERT INTO quiz (question,answer,correct_answer) VALUES (?,?,?)";
+    const values = [question,options,singleres.correct_answer];
+
+    connection.query(sqlQuery, values, (err, result) => {
+      if (err) {
+        console.error("Error inserting data into MySQL:", err);
+        return;
+      }
+      console.log("Data inserted into MySQL table successfully");
+    });
+  });
+
   return JSON.stringify(responsejs);
 }
 const server = http.createServer((req, res) => {
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,13 +1,1 @@
-if (path === '/') {
-    //     res.statusCode = 200;
-    //     res.setHeader('Content-Type', 'text/plain');
-    //     res.end('Hello, World!\n');
-    // } else if (path === '/fetch') {
-    //     res.statusCode = 200;
-    //     res.setHeader('Content-Type', 'text/plain');
-    //     res.end('Fetching data...\n');
-    // } else {
-    //     res.statusCode = 404;
-    //     res.setHeader('Content-Type', 'text/plain');
-    //     res.end('404 Not Found\n');
-    // }
+ // options+=singleres.correct_answer;
